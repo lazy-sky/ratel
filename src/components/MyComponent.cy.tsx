c4b32c6af58bde0d6f990b6f52343824ff164e77
lazy-sky <1cielo@naver.com>
2023-03-09 12:08:52 +0900

    fix: typo correction

--- src/components/MyComponent.cy.tsx
+++ src/components/MyComponent.cy.tsx
@@ -1,10 +1,10 @@
 import { mount } from 'cypress/react18';
 
-import TestComponent from './TestComponent';
+import MyComponent from './MyComponent';
 
 describe('MyComponent', () => {
   it('shows test text', () => {
-    mount(<TestComponent />);
+    mount(<MyComponent />);
     cy.get('div').should('contain.text', 'This is my component');
   });
 });
