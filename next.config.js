5b3f16c6998d97c901a8be876be6cde43f559031
lazy-sky <1cielo@naver.com>
2023-03-14 11:15:50 +0900

    ci: build docker image
    JIRA: TB-1039

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: '.next',
+  swcMinify: false,
+  experimental: {
+    forceSwcTransforms: false,
+  },
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
